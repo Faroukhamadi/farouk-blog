a69413b865528b6713d7c06b63b15fe7822181ef
Faroukhamadi <faroukhamadi1@outlook.com>
2022-09-25 23:51:59 +0100

    add links page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import prefetch from "@astrojs/prefetch";
+import tailwind from '@astrojs/tailwind';
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch()]
+	integrations: [tailwind(), prefetch()],
 });
