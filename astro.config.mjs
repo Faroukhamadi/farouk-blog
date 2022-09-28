f788ede68eb7e27bb35ce1358d5d2edcf0be81be
Faroukhamadi <faroukhamadi1@outlook.com>
2022-09-28 18:03:25 +0100

    add md for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import prefetch from '@astrojs/prefetch';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), prefetch()],
+  integrations: [tailwind(), prefetch()],
+  output: "server",
+  adapter: netlify()
 });
