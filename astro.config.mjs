00a01ceee0502485374aed41ca9a0e8ed7d17df3
Faroukhamadi <faroukhamadi1@outlook.com>
2022-09-30 17:50:16 +0100

    modify for deploy2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import prefetch from '@astrojs/prefetch';
 
-import netlify from '@astrojs/netlify/functions';
-
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), prefetch()],
-
-	// reset when you deploy to production
-	output: 'server',
-	adapter: netlify(),
 });
