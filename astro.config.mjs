7d535821a88a0db107b5e1ca27e03ecd20fa9e58
Faroukhamadi <faroukhamadi1@outlook.com>
2022-10-27 15:00:52 +0100

    add prod fix attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://farouk-blog.vercel.app/blog',
+	site: 'https://farouk-blog.vercel.app',
 
 	integrations: [tailwind(), prefetch(), robotsTxt()],
 });
