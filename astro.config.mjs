433bdbf2d067ef4ed1b615bff30adf9b12ba813a
Faroukhamadi <faroukhamadi1@outlook.com>
2022-10-27 14:54:30 +0100

    improve seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import prefetch from '@astrojs/prefetch';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), prefetch()],
+	site: 'https://farouk-blog.vercel.app/blog',
+
+	integrations: [tailwind(), prefetch(), robotsTxt()],
 });
